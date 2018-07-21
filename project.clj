1860e49d233eab0086626f9b9b878ca42d5d3ed4
Simon Brooke <simon@journeyman.cc>
2018-07-21 11:06:10 +0100

    Preparation for release

--- project.clj
+++ project.clj
@@ -1,9 +1,29 @@
 (defproject lein-adl "0.1.0-SNAPSHOT"
   :description "Integrate Application Description Language into lein workflow"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :url "https://github.com/simon-brooke/lein-adl"
+  :license {:name "Eclipse Public License" ;; for compatibility with leiningen
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[adl-support "0.1.1-SNAPSHOT"]
-                   [adl "1.4.2-SNAPSHOT"]]
+  :dependencies [[adl-support "0.1.3"]
+                 [adl "1.4.2"]]
+                   
+  :plugins [[lein-codox "0.10.3"]
+            [lein-release "1.0.5"]]
+
+  ;; `lein release` doesn't work with `git flow release`. To use
+  ;; `lein release`, first merge `develop` into `master`, and then, in branch
+  ;; `master`, run `lein release`
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["clean"]
+                  ["test"]
+                  ["codox"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ;; ["vcs" "tag"] -- not working, problems with secret key
+                  ["uberjar"]
+                  ["install"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]]
 
   :eval-in-leiningen true)
