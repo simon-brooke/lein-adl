f66527c1dd71b28fa3fe3b28e9a55bc84a8d09d9
Simon Brooke <simon@journeyman.cc>
2018-07-21 18:54:06 +0100

    Upversion to 0.1.2, using adl 1.4.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-adl "0.1.1"
+(defproject lein-adl "0.1.2"
   :description "Integrate Application Description Language into lein workflow"
   :url "https://github.com/simon-brooke/lein-adl"
   :license {:name "Eclipse Public License" ;; for compatibility with leiningen
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[adl-support "0.1.3"]
-                 [adl "1.4.2"]]
+                 [adl "1.4.3"]]
                    
   :plugins [[lein-codox "0.10.3"]
             [lein-release "1.0.5"]]
@@ -13,17 +13,20 @@
   ;; `lein release`, first merge `develop` into `master`, and then, in branch
   ;; `master`, run `lein release`
 
-  :release-tasks [["vcs" "assert-committed"]
-                  ["clean"]
-                  ["test"]
-                  ["codox"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ;; ["vcs" "tag"] -- not working, problems with secret key
-                  ["uberjar"]
-                  ["install"]
-                  ["deploy" "clojars"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]]
+  ;; in fact, `lein release` is pretty comprehensively broken, which is a
+  ;; shame because it's a good idea.
+
+  ;; :release-tasks [["vcs" "assert-committed"]
+  ;;                 ["clean"]
+  ;;                 ["test"]
+  ;;                 ["codox"]
+  ;;                 ["change" "version" "leiningen.release/bump-version" "release"]
+  ;;                 ["vcs" "commit"]
+  ;;                 ;; ["vcs" "tag"] -- not working, problems with secret key
+  ;;                 ["uberjar"]
+  ;;                 ["install"]
+  ;;                 ["deploy" "clojars"]
+  ;;                 ["change" "version" "leiningen.release/bump-version"]
+  ;;                 ["vcs" "commit"]]
 
   :eval-in-leiningen true)
