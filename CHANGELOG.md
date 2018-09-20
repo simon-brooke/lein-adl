823bae80d69ae2e17c7f39ed6b82302241b90136
Simon Brooke <simon@journeyman.cc>
2018-09-20 11:36:35 +0100

    Bumped versions of ADL files to current releases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,6 @@
 # Change Log
-All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+All notable changes to this project will be documented in this file. This change log is intended to follow the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## [Unreleased]
+## 0.1.4, 2018-09-20
 
-
-[Unreleased]: https://github.com/your-name/lein-adl/compare/0.1.1...HEAD
-[0.1.1]: https://github.com/your-name/lein-adl/compare/0.1.0...0.1.1
+This is not the official beta release, but it is a dummy run for a beta release.
--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/simon-brooke/lein-adl"
   :license {:name "Eclipse Public License" ;; for compatibility with leiningen
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[adl-support "0.1.3"]
-                 [adl "1.4.3"]]
+  :dependencies [[adl-support "0.1.4"]
+                 [adl "1.4.5"]]
 
   :plugins [[lein-codox "0.10.3"]
             [lein-release "1.0.5"]]
@@ -12,12 +12,8 @@
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
 
-  ;; `lein release` doesn't work with `git flow release`. To use
-  ;; `lein release`, first merge `develop` into `master`, and then, in branch
-  ;; `master`, run `lein release`
-
-  ;; in fact, `lein release` is pretty comprehensively broken, which is a
-  ;; shame because it's a good idea.
+  ;; `lein release` doesn't play nice with `git flow release`. Run `lein release` in the
+  ;; `develop` branch, then merge the release tag into the `master` branch.
 
   :release-tasks [["vcs" "assert-committed"]
                   ["clean"]
