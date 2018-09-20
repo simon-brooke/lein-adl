8502384cbe140ecefeff68bfb3a31fad89c2f834
Simon Brooke <simon@journeyman.cc>
2018-09-20 10:21:40 +0100

    Upversioned to 0.1.3-SNAPSHOT, prior to trying to get release working

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-adl "0.1.2"
+(defproject lein-adl "0.1.3-SNAPSHOT"
   :description "Integrate Application Description Language into lein workflow"
   :url "https://github.com/simon-brooke/lein-adl"
   :license {:name "Eclipse Public License" ;; for compatibility with leiningen
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[adl-support "0.1.3"]
                  [adl "1.4.3"]]
-                   
+
   :plugins [[lein-codox "0.10.3"]
             [lein-release "1.0.5"]]
 
@@ -16,17 +16,17 @@
   ;; in fact, `lein release` is pretty comprehensively broken, which is a
   ;; shame because it's a good idea.
 
-  ;; :release-tasks [["vcs" "assert-committed"]
-  ;;                 ["clean"]
-  ;;                 ["test"]
-  ;;                 ["codox"]
-  ;;                 ["change" "version" "leiningen.release/bump-version" "release"]
-  ;;                 ["vcs" "commit"]
-  ;;                 ;; ["vcs" "tag"] -- not working, problems with secret key
-  ;;                 ["uberjar"]
-  ;;                 ["install"]
-  ;;                 ["deploy" "clojars"]
-  ;;                 ["change" "version" "leiningen.release/bump-version"]
-  ;;                 ["vcs" "commit"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["clean"]
+                  ["test"]
+                  ["codox"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ;; ["vcs" "tag"] -- not working, problems with secret key
+                  ["uberjar"]
+                  ["install"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]]
 
   :eval-in-leiningen true)
